bb34c77dce9ceeb10157970e992cb540656d5410
Seokseong Jeon <6747391+gsongsong@users.noreply.github.com>
2019-03-23 19:38:58 +0900

    Add asn1.extract cli and force tslint no-console (#6)

--- src/asn1/extract.ts
+++ src/asn1/extract.ts
@@ -1,16 +1,19 @@
+import { readFile } from 'fs';
+
 interface IToken {
   start: RegExp;
   end: RegExp;
 }
 
 const tokens: { [protocol: string]: IToken} = {
-  rrc: {
+  RRC: {
     start: /^-- ASN1START/gm,
     end: /^-- ASN1STOP/gm,
   },
 };
 
 export function extract(text: string, protocol: string): string {
+  protocol = protocol.toUpperCase();
   const extractedTexts: string[] = [];
   while (true) {
     const matchStart = tokens[protocol].start.exec(text);
@@ -26,4 +29,17 @@ export function extract(text: string, protocol: string): string {
     extractedTexts.push(text.substring(matchStart.index + matchStart[0].length, matchEnd.index));
   }
   return extractedTexts.join('');
+}
+
+if (require.main === module) {
+  const [protocol, filePath] = process.argv.slice(2);
+  if (!protocol || !filePath) {
+    throw Error('Requires 2 arguments, protocol and filePath');
+  }
+  readFile(filePath, 'utf8', (err: Error, text: string) => {
+    if (err) {
+      throw err;
+    }
+    process.stdout.write(extract(text, protocol));
+  });
 }
