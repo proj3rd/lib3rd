a917e84facd2da895829ea9d9067a963b10fa015
Seokseong Jeon <sjeon87@gmail.com>
2019-03-24 18:41:56 +0900

    Add ans1 parser CLI

--- dist/asn1/parse.js
+++ dist/asn1/parse.js
@@ -1,8 +1,11 @@
 "use strict";
 exports.__esModule = true;
+var fs_1 = require("fs");
 var antlr4 = require("antlr4");
 var ASNLexer_1 = require("./ASNLexer");
 var ASNParser_1 = require("./ASNParser");
+var modules_1 = require("./visitor/modules");
+var text_1 = require("../utils/text");
 function parse(text) {
     var chars = new antlr4.InputStream(text);
     var lexer = new ASNLexer_1.ASNLexer(chars);
@@ -11,3 +14,15 @@ function parse(text) {
     parser.buildParseTrees = true;
 }
 exports.parse = parse;
+if (require.main === module) {
+    var filePath = process.argv[2];
+    if (!filePath) {
+        throw Error('Requires an argument, filePath');
+    }
+    fs_1.readFile(filePath, 'utf8', function (err, text) {
+        if (err) {
+            throw err;
+        }
+        process.stdout.write(JSON.stringify(parse(text_1.sanitize(text)), null, 2));
+    });
+}
--- src/asn1/parse.ts
+++ src/asn1/parse.ts
@@ -1,8 +1,12 @@
-import * as antlr4 from 'antlr4';
+import { readFile } from 'fs';
 
+import * as antlr4 from 'antlr4';
 import { ASNLexer } from './ASNLexer';
 import { ASNListener } from './ASNListener';
 import { ASNParser } from './ASNParser';
+import { ModulesVisitor } from './visitor/modules';
+
+import { sanitize } from '../utils/text';
 
 export function parse(text: string): any /* TODO */ {
   const chars = new antlr4.InputStream(text);
@@ -10,4 +14,17 @@ export function parse(text: string): any /* TODO */ {
   const tokens = new antlr4.CommonTokenStream(lexer);
   const parser = new ASNParser(tokens);
   parser.buildParseTrees = true;
+}
+
+if (require.main === module) {
+  const filePath = process.argv[2];
+  if (!filePath) {
+    throw Error('Requires an argument, filePath');
+  }
+  readFile(filePath, 'utf8', (err: Error, text: string) => {
+    if (err) {
+      throw err;
+    }
+    process.stdout.write(JSON.stringify(parse(sanitize(text)), null, 2));
+  });
 }
