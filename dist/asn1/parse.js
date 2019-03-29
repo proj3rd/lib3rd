f46aacc45826df37d7e7b696a084eb1a0a92b6bf
Seokseong Jeon <sjeon87@gmail.com>
2019-03-29 21:05:06 +0900

    Remove CLI

--- dist/asn1/parse.js
+++ dist/asn1/parse.js
@@ -1,27 +1,14 @@
 "use strict";
 exports.__esModule = true;
-var fs_1 = require("fs");
 var antlr4 = require("antlr4");
 var ASNLexer_1 = require("./ASNLexer");
 var ASNParser_1 = require("./ASNParser");
-var text_1 = require("../utils/text");
 function parse(text) {
     var chars = new antlr4.InputStream(text);
     var lexer = new ASNLexer_1.ASNLexer(chars);
     var tokens = new antlr4.CommonTokenStream(lexer);
     var parser = new ASNParser_1.ASNParser(tokens);
     parser.buildParseTrees = true;
+    return parser.modules();
 }
 exports.parse = parse;
-if (require.main === module) {
-    var filePath = process.argv[2];
-    if (!filePath) {
-        throw Error('Requires an argument, filePath');
-    }
-    fs_1.readFile(filePath, 'utf8', function (err, text) {
-        if (err) {
-            throw err;
-        }
-        process.stdout.write(JSON.stringify(parse(text_1.sanitize(text)), null, 2) + '\n');
-    });
-}
--- src/asn1/parse.ts
+++ src/asn1/parse.ts
@@ -1,10 +1,6 @@
-import { readFile } from 'fs';
-
 import * as antlr4 from 'antlr4';
 import { ASNLexer } from './ASNLexer';
 import { ASNParser } from './ASNParser';
-
-import { sanitize } from '../utils/text';
 
 export function parse(text: string): any {
   const chars = new antlr4.InputStream(text);
@@ -12,17 +8,5 @@ export function parse(text: string): any {
   const tokens = new antlr4.CommonTokenStream(lexer);
   const parser = new ASNParser(tokens);
   parser.buildParseTrees = true;
-}
-
-if (require.main === module) {
-  const filePath = process.argv[2];
-  if (!filePath) {
-    throw Error('Requires an argument, filePath');
-  }
-  readFile(filePath, 'utf8', (err: Error, text: string) => {
-    if (err) {
-      throw err;
-    }
-    process.stdout.write(JSON.stringify(parse(sanitize(text)), null, 2) + '\n');
-  });
+  return parser.modules();
 }
