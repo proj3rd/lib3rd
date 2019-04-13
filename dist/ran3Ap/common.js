7cb8cf358320036025e065a0b2af67ad5acb2073
Seokseong Jeon <sjeon87@gmail.com>
2019-04-14 03:05:52 +0900

    Fix reference number regexpp incorrectly matches

--- dist/ran3Ap/common.js
+++ dist/ran3Ap/common.js
@@ -2,5 +2,5 @@
 exports.__esModule = true;
 // In case of section information is not contained in heading tag (h1-h6)
 // Supports form of X.Y.Z and X.Y.Za
-exports.reSection = /^\d+(\.\d+)*?\.\d+\w?\s+?.+$/;
-exports.reReference = /\d+(\.\d+)*?\.\d+\w?/;
+exports.reSection = /^\b[1-9A-Z]\d*(\.[1-9]\d*)*\.[1-9]\d*\w*\b\s+?.+$/;
+exports.reReference = /\b[1-9A-Z]\d*(\.[1-9]\d*)*\.[1-9]\d*\w*\b/;
--- src/ran3Ap/common.ts
+++ src/ran3Ap/common.ts
@@ -1,4 +1,4 @@
 // In case of section information is not contained in heading tag (h1-h6)
 // Supports form of X.Y.Z and X.Y.Za
-export const reSection = /^\d+(\.\d+)*?\.\d+\w?\s+?.+$/;
-export const reReference = /\d+(\.\d+)*?\.\d+\w?/;
+export const reSection = /^\b[1-9A-Z]\d*(\.[1-9]\d*)*\.[1-9]\d*\w*\b\s+?.+$/;
+export const reReference = /\b[1-9A-Z]\d*(\.[1-9]\d*)*\.[1-9]\d*\w*\b/;
