4ddf46eea658eb51d98a91ee0e37fd816283fe0d
Seokseong Jeon <sjeon87@gmail.com>
2019-04-30 01:05:55 +0900

    Add newline

--- src/asn1/classes/asnBoolean.ts
+++ src/asn1/classes/asnBoolean.ts
@@ -2,6 +2,7 @@ export class AsnBoolean {
   public expand(): AsnBoolean {
     return this;
   }
+
   public toString(): string {
     return 'BOOLEAN';
   }
--- src/asn1/classes/null.ts
+++ src/asn1/classes/null.ts
@@ -2,6 +2,7 @@ export class Null {
   public expand(): Null {
     return this;
   }
+
   public toString(): string {
     return 'NULL';
   }
