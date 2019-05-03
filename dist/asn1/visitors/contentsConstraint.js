0335b1a62d56ead289017f7348dc829c69387c7c
Seokseong Jeon <sjeon87@gmail.com>
2019-05-04 02:09:02 +0900

    Parse CONTAINING constraint

--- dist/asn1/visitors/contentsConstraint.js
+++ dist/asn1/visitors/contentsConstraint.js
@@ -2,6 +2,8 @@
 exports.__esModule = true;
 var logging_1 = require("../../utils/logging");
 var utils_1 = require("../utils");
+var asnType_1 = require("./asnType");
+var value_1 = require("./value");
 /**
  * ANTLR4 grammar
  * ```contentsConstraint :
@@ -15,7 +17,34 @@ var ContentsConstraintVisitor = /** @class */ (function () {
     function ContentsConstraintVisitor() {
     }
     ContentsConstraintVisitor.prototype.visitChildren = function (contentsConstraintCtx) {
-        logging_1.log.warn(utils_1.getLogWithAsn1(contentsConstraintCtx, 'ContentsConstraint not supported:'));
+        var childCtxes = contentsConstraintCtx.children;
+        var contentsConstraint = {};
+        switch (childCtxes[0].getText().toLowerCase()) {
+            case 'containing': {
+                var asnTypeCtx = childCtxes[1];
+                var asnType = asnTypeCtx.accept(new asnType_1.AsnTypeVisitor());
+                contentsConstraint.containing = asnType;
+                var valueCtx = childCtxes[4];
+                if (valueCtx) {
+                    var value = valueCtx.accept(new value_1.ValueVisitor());
+                    contentsConstraint.encodedBy = value;
+                }
+                break;
+            }
+            case 'encoded': {
+                logging_1.log.warn(utils_1.getLogWithAsn1(contentsConstraintCtx, 'Encoded not supported:'));
+                break;
+            }
+            case 'with': {
+                logging_1.log.warn(utils_1.getLogWithAsn1(contentsConstraintCtx, 'With not supported:'));
+                break;
+            }
+            default: {
+                logging_1.log.warn(utils_1.getLogWithAsn1(contentsConstraintCtx, 'Not supported ASN1:'));
+                break;
+            }
+        }
+        return contentsConstraint;
     };
     return ContentsConstraintVisitor;
 }());
--- src/asn1/visitors/contentsConstraint.ts
+++ src/asn1/visitors/contentsConstraint.ts
@@ -1,6 +1,9 @@
 import { log } from '../../utils/logging';
 import { getLogWithAsn1 } from '../utils';
 
+import { AsnTypeVisitor } from './asnType';
+import { ValueVisitor } from './value';
+
 /**
  * ANTLR4 grammar
  * ```contentsConstraint :
@@ -12,6 +15,33 @@ import { getLogWithAsn1 } from '../utils';
  */
 export class ContentsConstraintVisitor {
   public visitChildren(contentsConstraintCtx: any): any /* TODO */ {
-    log.warn(getLogWithAsn1(contentsConstraintCtx, 'ContentsConstraint not supported:'));
+    const childCtxes = contentsConstraintCtx.children;
+    const contentsConstraint: any /* TODO */ = {};
+    switch (childCtxes[0].getText().toLowerCase()) {
+      case 'containing': {
+        const asnTypeCtx = childCtxes[1];
+        const asnType = asnTypeCtx.accept(new AsnTypeVisitor());
+        contentsConstraint.containing = asnType;
+        const valueCtx = childCtxes[4];
+        if (valueCtx) {
+          const value = valueCtx.accept(new ValueVisitor());
+          contentsConstraint.encodedBy = value;
+        }
+        break;
+      }
+      case 'encoded': {
+        log.warn(getLogWithAsn1(contentsConstraintCtx, 'Encoded not supported:'));
+        break;
+      }
+      case 'with': {
+        log.warn(getLogWithAsn1(contentsConstraintCtx, 'With not supported:'));
+        break;
+      }
+      default: {
+        log.warn(getLogWithAsn1(contentsConstraintCtx, 'Not supported ASN1:'));
+        break;
+      }
+    }
+    return contentsConstraint;
   }
 }
