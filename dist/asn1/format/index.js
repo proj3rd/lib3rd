4d26170e058454265914c2577efcb37626aef1cd
Seokseong Jeon <sjeon87@gmail.com>
2019-05-05 15:41:33 +0900

    Separate formatTxt

--- dist/asn1/format/index.js
+++ dist/asn1/format/index.js
@@ -2,24 +2,35 @@
 exports.__esModule = true;
 var fs_1 = require("fs");
 var path_1 = require("path");
-var parse_1 = require("./parse");
-/**
- * Format ASN.1
- * @param asn1 ASN.1 object you want to format
- * @param fmt Format you want to get. `txt` (default) is supported currently
- */
-function format(msgIeName, asn1, fmt) {
-    if (fmt === void 0) { fmt = 'txt'; }
-    switch (fmt) {
-        case 'txt': {
-            return msgIeName + " ::= " + asn1.toString();
+var text_1 = require("./text");
+var parse_1 = require("../parse");
+// TODO: need to be place in separate module?
+function findMsgIes(msgIeName, asn1) {
+    var msgIes = [];
+    Object.keys(asn1).forEach(function (moduleName) {
+        var assignments = asn1[moduleName].assignments;
+        if (msgIeName === 'all') {
+            Object.keys(assignments).forEach(function (name) {
+                msgIes.push({
+                    name: name,
+                    definition: assignments[name]
+                });
+            });
         }
-        default: {
-            throw Error("Format '" + fmt + "' not supported");
+        else {
+            if (msgIes.length) {
+                return;
+            }
+            if (msgIeName in assignments) {
+                msgIes.push({
+                    name: msgIeName,
+                    definition: assignments[msgIeName]
+                });
+            }
         }
-    }
+    });
+    return msgIes;
 }
-exports.format = format;
 if (require.main === module) {
     var _a = process.argv.slice(2), filePath_1 = _a[0], msgIeName_1 = _a[1];
     if (!filePath_1 || !msgIeName_1) {
@@ -33,22 +44,15 @@ if (require.main === module) {
         fmt = fmt ? fmt : 'txt';
         expand = expand ? expand : null;
         var parseResult = parse_1.parse(text);
-        var msgIeDefinition = null;
-        Object.keys(parseResult).forEach(function (moduleName) {
-            if (msgIeDefinition) {
-                return;
-            }
-            if (msgIeName_1 in parseResult[moduleName].assignments) {
-                msgIeDefinition = parseResult[moduleName].assignments[msgIeName_1];
-            }
-        });
-        if (!msgIeDefinition) {
+        var msgIes = findMsgIes(msgIeName_1, parseResult);
+        if (!msgIes.length) {
             throw Error(msgIeName_1 + " not found in " + filePath_1);
         }
-        var formatResult = format(msgIeName_1, msgIeDefinition, fmt);
+        // TODO: expand
         var parsedPath = path_1.parse(filePath_1);
         switch (fmt) {
             case 'txt': {
+                var formatResult = text_1.format(msgIes);
                 fs_1.writeFileSync(msgIeName_1 + "-" + parsedPath.name + ".txt", formatResult);
                 break;
             }
---
+++ dist/asn1/format/text.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var logging_1 = require("../../utils/logging");
+/**
+ * Format ASN.1 in text
+ * @param msgIeDefinition ASN.1 object you want to format
+ */
+function format(msgIes) {
+    var formattedStrings = [];
+    msgIes.forEach(function (msgIe) {
+        logging_1.log.debug("Formatting " + msgIe.name + " in text...");
+        formattedStrings.push(msgIe.name + " ::= " + msgIe.definition.toString());
+    });
+    return msgIes.join('\n\n');
+}
+exports.format = format;
