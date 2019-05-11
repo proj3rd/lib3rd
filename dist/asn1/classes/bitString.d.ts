09a167a87675f640a85cee73fea407b12b1fb7ed
Seokseong Jeon <sjeon87@gmail.com>
2019-05-11 16:55:20 +0900

    Implement bitString.fillWorksheet()

--- dist/asn1/classes/bitString.d.ts
+++ dist/asn1/classes/bitString.d.ts
@@ -1,3 +1,4 @@
+import { IFormatConfig, IIe } from '../format/xlsx';
 import { Base } from './base';
 export declare class BitString extends Base {
     namedBitList: any;
@@ -8,4 +9,5 @@ export declare class BitString extends Base {
     expand(): BitString;
     depthMax(): number;
     toString(): string;
+    fillWorksheet(ieElem: IIe, ws: any, row: number, col: number, depthMax: number, constants: any[], formatConfig: IFormatConfig, depth?: number): [number, number];
 }
