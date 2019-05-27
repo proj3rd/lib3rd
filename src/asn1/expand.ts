bcc7c7b33a0681739f1f1fadbd70fd2fe1f37bdc
Seokseong Jeon <sjeon87@gmail.com>
2019-05-27 22:50:18 +0900

    Fix expand entry point

--- src/asn1/expand.ts
+++ src/asn1/expand.ts
@@ -4,6 +4,6 @@ import { IMsgIe } from './format/common';
 
 export function expand(msgIe: IMsgIe, asn1Pool: any /* TODO */): any {
   const msgIeClone = cloneDeep(msgIe);
-  msgIeClone.definition.expand(asn1Pool);
+  msgIeClone.definition.expand(asn1Pool, msgIeClone.definition.moduleName);
   return msgIeClone;
 }
