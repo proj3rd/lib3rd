ceb5d456f0c231ebe9d410d81e0723bbc8e8cc02
Seokseong Jeon <sjeon87@gmail.com>
2019-08-20 14:52:12 +0900

    antlr4ts  ready: tag

--- src/asn1/visitors/tag.ts
+++ src/asn1/visitors/tag.ts
@@ -1,3 +1,8 @@
+import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor';
+
+import { TagContext } from '../ASN_3gppParser';
+import { ASN_3gppVisitor } from '../ASN_3gppVisitor';
+
 const reTag = /^-- *?(Need|Cond) *?.+?$/;
 
 /**
@@ -12,15 +17,16 @@ const reTag = /^-- *?(Need|Cond) *?.+?$/;
  *   ;
  * ```
  */
-export class TagVisitor {
-  public visitChildren(tagCtx: any): any /* TODO */ {
+export class TagVisitor extends AbstractParseTreeVisitor<string> implements ASN_3gppVisitor<string> {
+  public defaultResult(): string {
+    return undefined;
+  }
+
+  public visitChildren(tagCtx: TagContext): string {
     const childCtx = tagCtx.children[0];
     // FIXME: ASN_3gpp.g4 not working properly
     // Temporary workaround
-    const tag: string = childCtx.getText();
-    if (tag.match(reTag)) {
-      return tag;
-    }
-    return null;
+    const tag: string = childCtx.text;
+    return tag.match(reTag) ? tag : null;
   }
 }
