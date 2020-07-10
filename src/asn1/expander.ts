f120f265a8e74b2eb9184899f2ed2236e1b894b4
Seokseong Jeon <sjeon87@gmail.com>
2020-07-10 19:03:09 +0900

    Subdivide expander test casese into moduleName.assignmentName

--- src/asn1/expander.ts
+++ src/asn1/expander.ts
@@ -13,25 +13,31 @@ describe('Expand ASN.1 [expand_all]', function () {
   this.timeout(0);
 
   // tslint:disable-next-line: only-arrow-functions
-  it('LTE RRC (36331) [expand_36331]', function () {
+  describe('LTE RRC (36331) [expand_36331]', function () {
     const asn1 = readFileSync('resources/36331-g00.asn1', 'utf8');
     const modules = parse(asn1);
     modules.modules.forEach((moduleDefinition) => {
       const { assignments } = moduleDefinition;
       assignments.forEach((assignment) => {
-        assignment.expand(modules, []);
+        // tslint:disable-next-line: only-arrow-functions
+        it(`${moduleDefinition.name}.${assignment.name}`, function () {
+          assignment.expand(modules, []);
+        });
       });
     });
   });
 
   // tslint:disable-next-line: only-arrow-functions
-  it('NR RRC (38331) [expand_38331]', function () {
+  describe('NR RRC (38331) [expand_38331]', function () {
     const asn1 = readFileSync('resources/38331-g00.asn1', 'utf8');
     const modules = parse(asn1);
     modules.modules.forEach((moduleDefinition) => {
       const { assignments } = moduleDefinition;
       assignments.forEach((assignment) => {
-        assignment.expand(modules, []);
+        // tslint:disable-next-line: only-arrow-functions
+        it(`${moduleDefinition.name}.${assignment.name}`, function () {
+          assignment.expand(modules, []);
+        });
       });
     });
   });
